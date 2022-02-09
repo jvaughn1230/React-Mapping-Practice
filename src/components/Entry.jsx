c48b8269603673d432be29e3040fbb2ef0536fc1
jvaughn1230 <75887112+jvaughn1230@users.noreply.github.com>
2022-02-09 09:36:39 -0800

    Initial commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        {props.name}
+      </dt>
+      <dd>{props.description}</dd>
+    </div>
+  );
+}
+
+export default Entry;
